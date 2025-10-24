eace9f217ecfa4570518d5205039482b2445a2e5
WorstWorld2 <michailkoutouris@gmail.com>
2025-10-24 20:06:07 +0300

    Small change to include response.json()

--- script.py
+++ script.py
@@ -4,7 +4,11 @@ while True:
     url = input("Enter the URL: ")
     response = requests.get(url)
     if response.status_code == 200:
-        text = response.text
-        print("Site source code: ",text)
+        try:
+            text = response.json()
+            print("Response: ",text)
+        except:
+            text = response.text
+            print("Response: ",text)
     else:
         print("An error occured.")
